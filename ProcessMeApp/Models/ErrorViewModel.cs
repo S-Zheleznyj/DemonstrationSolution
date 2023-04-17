3375f99b9b2451e954787b86bcccc8a104d32870
Sergey Zheleznyy <sergo1.iron@gmail.com>
2023-04-17 15:37:46 +0300

    Удален лишний проект

--- ProcessMeApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProcessMeApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
